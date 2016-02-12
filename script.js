7fd3ba92cd372dd9ebbde6f9d1fe2aca80566207
ivsasa1516daw2 <ivsasa1516daw2@gmail.com>
2016-02-12 17:37:25 +0100

    logica feta

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+//cloneNode permet fer còpies dels Node, mentre que appendChild els mou
+function mostraSerie(serie){
+    document.getElementById("div1").appendChild(serie[0].cloneNode(true));
+    document.getElementById("div2").appendChild(serie[1].cloneNode(true));
+    document.getElementById("div3").appendChild(serie[2].cloneNode(true));
+}
+
+//Retorna el valor del comptador de sèries guardades en sessionStorage
+function getComptador(){
+    if (sessionStorage.counter){
+        var count = Number(sessionStorage.counter);
+     }
+    else {
+        sessionStorage.counter=0;
+        var count = Number(sessionStorage.counter);
+     }
+    return count;
+}
+
 window.onload = function () {
+    var count = getComptador(); //recuperem el valor del contador o el posem a 0 si es la primera sèrie
+
+
+    console.log(count);
+
+    //comprovació de si hem acabat totes les sèries
+    if(count >= 4){
+
+    }
 
      var divs = document.querySelectorAll('div');
     [].forEach.call(divs, function (item) {
@@ -25,10 +53,21 @@ window.onload = function () {
 
         var data = ev.dataTransfer.getData("imatge");
         if ( event.target.nodeName !== "IMG" ) {    //evita que les imatges es fiquin dins d'elles mateixes
-            ev.target.appendChild(document.getElementById(data));
+            if(document.getElementById(data).src == series[count].pop().src){ //Comprova que sigui la imatge correcte segons està definida a la sèrie
+                document.getElementById("resultat").childNodes[0].nodeValue = "Correcte"; //Informa a l'usuari
+                ev.target.appendChild(document.getElementById(data));
+                sessionStorage.counter=count+1; //Objecte que guarda en emmagatzematge web el valor de count que serveix per avançar en les sèries i veure quan s'han acabat
+                location.reload(false);
+            }
+            else {
+                document.getElementById("resultat").childNodes[0].nodeValue = "Incorrecte"; //Informa a l'usuari
+            }
         }
     }
 
+
+
+    //Precàrrega d'imatges
     var image1 = new Image();
     image1.src = 'images/image1.png';
     var image2 = new Image();
@@ -38,10 +77,15 @@ window.onload = function () {
     var image4 = new Image();
     image4.src = 'images/image4.png';
 
-    var serie1 = [image1, image2, image3, image4];
+    var serie1 = [image1, image1, image1, image1];
+    var serie2 = [image3, image4, image3, image4];
+    var serie3 = [image2, image2, image4, image4];
+    var serie4 = [image3, image4, image3, image4];
 
-    document.getElementById("div1").appendChild(serie1[0]);
-    document.getElementById("div2").appendChild(serie1[1]);
-    document.getElementById("div3").appendChild(serie1[2]);
+    var series = [serie1, serie2, serie3, serie4];
+
+    mostraSerie(series[count]);
+
+
 
 };
